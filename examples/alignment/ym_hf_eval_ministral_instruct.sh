b21bdf054f092bd8404d325fb7eabb8a262f3e70
Yannick Metz <ymetz@clariden-ln003.cscs.ch>
2026-02-12 15:40:59 +0100

    updated container

--- examples/alignment/ym_hf_eval_ministral_instruct.sh
+++ examples/alignment/ym_hf_eval_ministral_instruct.sh
@@ -1,3 +1,3 @@
-MODEL=Ministral-3-OB
-CKPT_PATH=mistralai/Ministral-3-8B-Instruct-2512
+MODEL=Ministral-8B-Instruct-2410
+CKPT_PATH=mistralai/Ministral-8B-Instruct-2410
 sbatch --job-name eval-$MODEL scripts/ym_evaluate_hf.sbatch $CKPT_PATH $MODEL
